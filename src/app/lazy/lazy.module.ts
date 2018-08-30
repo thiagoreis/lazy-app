bfaad0c5d6ef511ae01fdc0e85cd928b0b190ed9
Thiago Reis <thiagoreis128@gmail.com>
2018-08-30 14:40:23 -0300

    Criando o LazyModule

---
+++ src/app/lazy/lazy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LazyRoutingModule } from './lazy-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LazyRoutingModule
+  ],
+  declarations: []
+})
+export class LazyModule { }
